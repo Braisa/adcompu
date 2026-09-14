8e31b7643ff581f272e01ca297bd69f52710ae76
Braisa <oterolemabrais@gmail.com>
2026-09-14 09:31:57 +0200

    Diego - Day 4 - Checking uncertainties

---
+++ diego/day-4/checking_uncertainties.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATA_FILE = "diego/day-4/data0c.txt"
+
+data = np.loadtxt(DATA_FILE, unpack=True)
+
+fig, ax = plt.subplots()
+
+ax.errorbar(*data, capsize=4, color="tab:orange", fmt="o", ls=None)
+
+fig.savefig(f"diego/day-4/checking_uncertainties_{DATA_FILE.split("/")[-1].split(".")[0]}.pdf", bbox_inches="tight")
