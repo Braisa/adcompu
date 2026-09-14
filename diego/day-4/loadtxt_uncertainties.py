4af01904a8a7c0d86e6e7506f92a058fb411d379
Braisa <oterolemabrais@gmail.com>
2026-09-14 09:17:44 +0200

    Diego - Day 4 - Loadtxt uncertainties

---
+++ diego/day-4/loadtxt_uncertainties.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+DATA_FILE = "diego/day-4/data0c.txt"
+
+x, y, sy = np.loadtxt(DATA_FILE, unpack=True)
+
+fig, ax = plt.subplots()
+
+ax.errorbar(x, y, yerr=sy, capsize=4, color="tab:orange", fmt="o", ls=None)
+
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$y$")
+
+fig.savefig("diego/day-4/loadtxt_uncertainties.pdf", bbox_inches="tight")
