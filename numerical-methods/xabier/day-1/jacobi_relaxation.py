6977c463b28dd495123a2800efe364d5e3191a0d
Braisa <oterolemabrais@gmail.com>
2026-09-21 10:02:39 +0200

    Xabier - Day 1 - Jacobi relaxation

---
+++ numerical-methods/xabier/day-1/jacobi_relaxation.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from cmasher import get_sub_cmap
+from tqdm import tqdm
+
+def jacobi_step(V):
+    V_new = V.copy()
+    V_new[1:-1, 1:-1] = .25 * (V[1:-1, :-2] + V[1:-1, 2:] + V[:-2, 1:-1] + V[2:, 1:-1])
+    return V_new
+
+maximum_steps = 50
+tolerance = 1e-5
+
+side = 5
+V_initial = np.zeros((side, side))
+V_initial[0,:] = 1
+
+V_prev = V_initial
+
+for step in tqdm(range(maximum_steps)):
+
+    V_step = jacobi_step(V_prev)
+
+    below_tolerance = np.all(np.abs(V_step - V_prev) < tolerance)
+    if not below_tolerance:
+        V_prev = V_step
+    else:
+        break
+
+fig, ax = plt.subplots()
+
+ax.imshow(V_step, cmap=get_sub_cmap("viridis", .2, .8))
+
+ax.set_xticks(np.arange(side))
+ax.set_yticks(np.arange(side))
+
+ax.set_xticklabels(np.arange(side))
+ax.set_yticklabels(np.arange(side))
+
+fig.savefig("numerical-methods/xabier/day-1/jacobi_relaxation.pdf", bbox_inches="tight")
